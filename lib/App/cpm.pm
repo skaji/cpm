8b5d1ef28c9802f9dcd8e127a1169abd73bd4ad0
Shoichi Kaji <skaji@cpan.org>
2015-10-18 02:52:41 +0900

    Now fix error which local::lib 2.000017 introduced

--- lib/App/cpm.pm
+++ lib/App/cpm.pm
@@ -155,12 +155,16 @@ sub cmd_install {
         version => $_->{version} || 0
     ) for @package;
 
-    # prevent local::lib 2.000017 error
+    # prevent local::lib 2.000017+ error
     # Unable to create /home/skaji/local/lib/perl5: File exists at /home/skaji/env/plenv/versions/c5.8.5/lib/perl5/site_perl/5.8.5/local/lib.pm line 678.
+    # See https://github.com/Perl-Toolchain-Gang/local-lib/commit/2f7bd47d1098d032de6d5f61354e310ad41b1097
     if (!$self->{global}) {
-        my $lib = "$self->{local_lib}/lib/perl5/$Config{archname}";
-        my $bin = "$self->{local_lib}/bin";
-        mkpath $_ for grep !-d, $lib, $bin;
+        my $v = $Config{version};
+        $v =~ s/^v//;
+        my $lib_v = "$self->{local_lib}/lib/perl5/$v/$Config{archname}";
+        my $lib   = "$self->{local_lib}/lib/perl5/$Config{archname}";
+        my $bin   = "$self->{local_lib}/bin";
+        mkpath $_ for grep !-d, $lib_v, $lib, $bin;
     }
 
     $master->spawn_worker($cb) for 1 .. $self->{workers};
