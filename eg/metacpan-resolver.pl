996bd260d2faff9c796c61faa1a247140668a0df
Shoichi Kaji <skaji@cpan.org>
2016-10-24 02:16:39 +0900

    remove eg

--- eg/metacpan-resolver.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use App::cpm::Resolver::Cascade;
-use App::cpm::Resolver::MetaCPAN;
-
-my $cascade = App::cpm::Resolver::Cascade->new;
-$cascade->add(App::cpm::Resolver::MetaCPAN->new);
-$cascade;
