bdd02e4344b82a740270a6df7d1513df70fe9afc
Shoichi Kaji <skaji@cpan.org>
2017-06-27 08:44:39 +0900

    simplify prebuilt_base

--- lib/App/cpm/Worker.pm
+++ lib/App/cpm/Worker.pm
@@ -30,18 +30,8 @@ sub new {
 sub prebuilt_base {
     my ($class, $home) = @_;
 
-    # XXX Taking account of relocatable perls, we use the absolute path of perl
-    my $perlpath = "";
-    for my $this_perl ($^X, $Config{perlpath}) {
-        # See perldoc perlvar for $^X
-        if ($^O ne 'VMS') {
-            $this_perl .= $Config{_exe} unless $this_perl =~ m/$Config{_exe}$/i;
-        }
-        $perlpath = eval { Cwd::abs_path($this_perl) };
-        last if $perlpath
-    }
-
-    my $identity = $perlpath . Config->myconfig;
+    # XXX Taking account of relocatable perls, we also use $Config{startperl}
+    my $identity = $Config{startperl} . Config->myconfig;
     my $digest = Digest::MD5::md5_hex($identity);
     $digest = substr $digest, 0, 8;
     "$home/builds/$Config{version}-$Config{archname}-$digest";
