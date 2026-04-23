61c1c89821138b4a91861cc85110cd2355f06ca2
Shoichi Kaji <skaji@cpan.org>
2026-04-23 16:29:46 +0200

    Introduce a prebuilt builder (#280)

---
+++ lib/App/cpm/Builder/Prebuilt.pm
@@ -0,0 +1,50 @@
+package App::cpm::Builder::Prebuilt;
+use v5.24;
+use warnings;
+use experimental qw(signatures);
+
+use parent 'App::cpm::Builder::Base';
+
+use Config;
+use ExtUtils::Install ();
+use ExtUtils::InstallPaths ();
+use File::Spec;
+
+sub test ($self, $ctx) {
+    die ref($self) . " does not implement test";
+}
+
+sub install ($self, $ctx) {
+    my $install_base = $self->{install_base};
+
+    $self->run_install($ctx, sub {
+        $ctx->log("Copying prebuilt blib");
+        my $paths = ExtUtils::InstallPaths->new(
+            dist_name => $self->meta->name,
+            $install_base ? (install_base => $install_base) : (),
+        );
+        my $install_base_meta = $install_base ? File::Spec->catdir($install_base, "lib", "perl5") : $Config{sitelibexp};
+        my $meta_target_dir = File::Spec->catdir($install_base_meta, $Config{archname}, ".meta", $self->{distvname});
+
+        open my $fh, ">", \my $stdout;
+        {
+            local *STDOUT = $fh;
+            ExtUtils::Install::install([
+                from_to => $paths->install_map,
+                verbose => 0,
+                dry_run => 0,
+                uninstall_shadows => 0,
+                skip => undef,
+                always_copy => 1,
+                result => \my %result,
+            ]);
+            ExtUtils::Install::install({
+                'blib/meta' => $meta_target_dir,
+            });
+        }
+        $ctx->log($stdout);
+        return 1;
+    });
+}
+
+1;
